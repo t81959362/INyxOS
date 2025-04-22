77e33a8c440797bbbb3b3a7189e9b16d48e852c6
Penelope <t81959362@gmail.com>
2025-04-22 05:57:36 +0100

    Deploy via docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/INyxOS/',
   plugins: [react()],
   server: {
     port: 5173
-  }
+  },
+  build: { outDir: 'docs' }
 });
